abe3cb683a8100c726c1100d23c9a13f17d5a148
pbrn3 <58267268+pbrn3@users.noreply.github.com>
2020-03-23 14:34:53 +0100

    Modification

--- mysite/app.py
+++ mysite/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 
 import shutil
-from tkinter.filedialog import *  # Depuis tkinter import tout le contenu de filedialog
 from PIL import Image  # Depuis le module PIL, importer image, permet d'effectuer des actions sur des images
 import os
 
@@ -12,9 +11,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/SomeFunction')
 def SomeFunction():
-    nom_image = askopenfilename(title=u"Ouvrir votre image",filetypes=[('images png','.png'),('image jpg','.jpg'),('images jpeg','.jpeg'),('images ppm','.ppm'),('images gif','.gif'),('images tiff','.tiff'),('images bmp','.bmp')])
+    nom_image = "Entrez le chemin de l'image avec son nom et format : "
     img = Image.open(nom_image)
     l, h = img.size
     r, g, b = img.split()
@@ -22,9 +22,9 @@ def SomeFunction():
     green = list(g.getdata())
     blue = list(b.getdata())
     red_sum = sum(red)
-    green_sum =sum(green)
+    green_sum = sum(green)
     blue_sum = sum(blue)
-    taille=[red_sum, green_sum, blue_sum]
+    taille = [red_sum, green_sum, blue_sum]
     taille.sort(reverse=False)
     if taille[0] == red_sum:
         couleur_acc = list(r.getdata())
@@ -44,18 +44,19 @@ def SomeFunction():
         couleur_acc[j] = 2 * int(couleur_acc[j] // 2) + int(v[j])
     for k in range(8 * u):
         couleur_acc[k + 8] = 2 * int(couleur_acc[k + 8] // 2) + int(a[k])
-    couleur_modifiee = Image.new("L", (l,h))
-    couleur_modifiee .putdata(couleur_acc)
+    couleur_modifiee = Image.new("L", (l, h))
+    couleur_modifiee.putdata(couleur_acc)
     if taille[0] == red_sum:
         imgnew = Image.merge('RGB', (couleur_modifiee, g, b))
     if taille[0] == green_sum:
         imgnew = Image.merge('RGB', (r, couleur_modifiee, b))
     if taille[0] == blue_sum:
         imgnew = Image.merge('RGB', (r, g, couleur_modifiee))
-    nom_repertoire = askdirectory(initialdir="/",title='Choix du répertoire')
+    nom_repertoire = "Entrez le répertoire dans lequel vous souhaitez enregistrer l'image : "
     imgnew.save("image_avec_message_codé.png")
     shutil.copy2("image_avec_message_codé.png", nom_repertoire)
     os.remove("image_avec_message_codé.png")
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
